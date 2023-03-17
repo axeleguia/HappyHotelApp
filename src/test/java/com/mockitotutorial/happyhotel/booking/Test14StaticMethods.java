43e8f5ac93c0b85c95534617f719a51fd1a848eb
Axel Leguía <axeleguia.chero@outlook.com>
2023-03-17 00:15:37 -0500

    Updated git tests

---
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test14StaticMethods.java
@@ -0,0 +1,44 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.mockStatic;
+
+@ExtendWith(MockitoExtension.class)
+public class Test14StaticMethods {
+
+    @InjectMocks
+    private BookingService bookingService;
+    @Mock
+    private PaymentService paymentServiceMock;
+    @Mock
+    private RoomService roomServiceMock;
+    @Spy
+    private BookingDAO bookingDAOMock;
+    @Mock
+    private MailSender mailSenderMock;
+    @Captor
+    private ArgumentCaptor<Double> doubleCaptor;
+
+    @Test
+    void should_CalculateCorrectPrice() {
+        // given
+        try (MockedStatic<CurrencyConverter> mockedConverter = mockStatic(CurrencyConverter.class)) {
+            BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 05), 2, false);
+            double expected = 400;
+            mockedConverter.when(() -> CurrencyConverter.toEuro(anyDouble())).thenReturn(400.0);
+            // when
+            double actual = bookingService.calculatePriceEuro(bookingRequest);
+            // then
+            assertEquals(expected, actual);
+        }
+    }
+
+}
